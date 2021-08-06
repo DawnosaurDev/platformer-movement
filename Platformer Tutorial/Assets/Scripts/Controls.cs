af5d532fcd8489d8ea5fbc4ca1c821ede983d137
Dawnosaur <citybuilder14@gmail.com>
2021-08-06 12:06:44 +0100

    Added Unity Project

---
+++ Platformer Tutorial/Assets/Scripts/Controls.cs
@@ -0,0 +1,212 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""3adfb362-5bad-4f6e-a9e5-ea8860aec6e7"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""db4f223e-9213-4635-9111-5392980bf47c"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""cdcbe9f5-cdc3-4a4d-b1f8-0fe58e3d5d45"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""JumpUp"",
+                    ""type"": ""Button"",
+                    ""id"": ""af7ad025-0782-4dd8-9c8f-75714f3d996f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=1)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""f5231587-ec14-4ae0-930c-2b1c0e60ed99"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""222c2c08-f0cc-40df-9c7a-cd45337e5b55"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""7e2eb931-e838-454a-9ba9-4e7c0fa0568c"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5d92227b-2fca-4d24-8f13-0e3d57a94f0d"",
+                    ""path"": ""<Keyboard>/c"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d2263cba-87d5-4eb0-89c4-1972329d9781"",
+                    ""path"": ""<Keyboard>/c"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""JumpUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_JumpUp = m_Player.FindAction("JumpUp", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_JumpUp;
+    public struct PlayerActions
+    {
+        private @Controls m_Wrapper;
+        public PlayerActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @JumpUp => m_Wrapper.m_Player_JumpUp;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @JumpUp.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJumpUp;
+                @JumpUp.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJumpUp;
+                @JumpUp.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJumpUp;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @JumpUp.started += instance.OnJumpUp;
+                @JumpUp.performed += instance.OnJumpUp;
+                @JumpUp.canceled += instance.OnJumpUp;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnJumpUp(InputAction.CallbackContext context);
+    }
+}
---
+++ Platformer Tutorial/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[Header("References")]
+	private Rigidbody2D rb;
+
+	[Header("Movement")]
+	public float moveSpeed;
+	public float acceleration;
+	public float decceleration;
+	public float velPower;
+	[Space(10)]
+	public float frictionAmount;
+
+	[Header("Jump")]
+	public float jumpForce;
+	[Range(0,1)]
+	public float jumpCutMultiplier;
+	[Space(10)]
+	public float jumpCoyoteTime;
+	private float lastGroundedTime;
+	public float jumpBufferTime;
+	private float lastJumpTime;
+	[Space(10)]
+	public float fallGravityMultiplier;
+	private float gravityScale;
+	[Space(10)]
+	private bool isJumping;
+
+	[Header("Checks")]
+	public Transform groundCheckPoint;
+	public Vector2 groundCheckSize;
+	[Space(10)]
+	public LayerMask groundLayer;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		InputHandler.instance.OnJumpPressed += args => OnJump(args);
+		InputHandler.instance.OnJumpReleased += args => OnJumpUp(args);
+
+		gravityScale = rb.gravityScale;
+	}
+
+	private void Update()
+	{
+		#region Checks
+		if (Physics2D.OverlapBox(groundCheckPoint.position, groundCheckSize, 0, groundLayer)) //checks if set box overlaps with ground
+		{
+			lastGroundedTime = jumpCoyoteTime; //if so sets the lastGrounded to coyoteTime
+		}
+
+		if(rb.velocity.y < 0)
+		{
+			isJumping = false;
+		}
+		#endregion
+
+		#region Jump
+		if (lastGroundedTime > 0 && lastJumpTime > 0 && !isJumping) //checks if was last grounded within coyoteTime and that jump has been pressed within bufferTime
+		{
+			Jump();
+		}
+		#endregion
+
+		#region Timer
+		lastGroundedTime -= Time.deltaTime;
+		lastJumpTime -= Time.deltaTime;
+		#endregion
+	}
+
+	private void FixedUpdate()
+	{
+		#region Run
+		float targetSpeed = InputHandler.instance.MoveInput * moveSpeed; //calculate the direction we want to move in and our desired velocity
+		float speedDif = targetSpeed - rb.velocity.x; //calculate difference between current velocity and desired velocity
+		float accelRate = (Mathf.Abs(targetSpeed) > 0.01f)? acceleration : decceleration;
+		float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif); //applies acceleration to speed difference, the raises to a set power so acceleration increases with higher speeds, finally multiplies by sign to reapply direction
+
+		rb.AddForce(movement * Vector2.right); //applies force force to rigidbody, multiplying by Vector2.right so that it only affects X axis 
+		#endregion
+
+		#region Friction
+		if (lastGroundedTime > 0 && Mathf.Abs(InputHandler.instance.MoveInput) < 0.01f)
+		{
+			float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
+			amount *= Mathf.Sign(rb.velocity.x);
+			rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
+		}
+		#endregion
+
+		#region Jump Gravity
+		if (rb.velocity.y < 0)
+		{
+			rb.gravityScale = gravityScale * fallGravityMultiplier;
+		}
+		else
+		{
+			rb.gravityScale = gravityScale;
+		}
+		#endregion
+	}
+
+	private void Jump()
+	{
+		rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		isJumping = true;
+	}
+
+	public void OnJump(InputHandler.InputArgs args)
+	{
+		lastJumpTime = jumpBufferTime;
+	}
+
+	public void OnJumpUp(InputHandler.InputArgs args)
+	{
+		if(rb.velocity.y > 0 && isJumping)
+		{
+			rb.AddForce(Vector2.down * rb.velocity.y * jumpCutMultiplier, ForceMode2D.Impulse);
+		}
+	}
+}
